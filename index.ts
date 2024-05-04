53440fb7f39a27e287267df332f12cb09946dec7
datta <codingdatta@gmail.com>
2024-05-04 19:10:27 +0530

    removed few bugs

--- index.ts
+++ index.ts
@@ -14,7 +14,6 @@ import ApiRouter from "./src/routes/ApiRouter.ts";
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
